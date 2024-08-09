8082b7981119ac414043748b0628f71c1370355d
Terrence Rosales <118107221+Romo3@users.noreply.github.com>
2024-08-09 11:58:23 -0400

    deployment

--- api/index.js
+++ api/index.js
@@ -1,49 +1,4 @@
-//PRODUCTION
-
-// import express from "express";
-// import dotenv from "dotenv";
-// import mongoose from "mongoose";
-// import userRoutes from "./routes/user.routes.js";
-// import authRoutes from "./routes/auth.routes.js";
-// import lineRoutes from "./routes/line.routes.js";
-// import productRoutes from "./routes/product.routes.js";
-// import cookieParser from "cookie-parser";
-// import cors from "cors";
-// dotenv.config();
-
-// const app = express();
-// app.use(cors());
-// app.use(express.json());
-// app.use(cookieParser());
-
-// mongoose
-//   .connect(process.env.MONGODB)
-//   .then(() => {
-//     console.log("DB connected");
-//     app.listen(process.env.PORT, () => {
-//       console.log(`Server is running on port ${process.env.PORT}`);
-//     });
-//   })
-//   .catch((err) => {
-//     console.log("DB connection failed", err);
-//   });
-
-// app.use("/api/user", userRoutes);
-// app.use("/api/auth", authRoutes);
-// app.use("/api/lines", lineRoutes);
-// app.use("/api/product", productRoutes);
-
-// app.use((err, req, res, next) => {
-//   const statusCode = err.statusCode || 500;
-//   const message = err.message || "Internal Server Error";
-//   res.status(statusCode).json({
-//     success: false,
-//     message,
-//     statusCode,
-//   });
-// });
-
-// DEPLOYMNENT
+// Production
 
 import express from "express";
 import dotenv from "dotenv";
@@ -54,8 +9,6 @@ import lineRoutes from "./routes/line.routes.js";
 import productRoutes from "./routes/product.routes.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import serverless from "serverless-http"; // Import serverless-http to handle Express app
-
 dotenv.config();
 
 const app = express();
@@ -67,6 +20,9 @@ mongoose
   .connect(process.env.MONGODB)
   .then(() => {
     console.log("DB connected");
+    app.listen(process.env.PORT, () => {
+      console.log(`Server is running on port ${process.env.PORT}`);
+    });
   })
   .catch((err) => {
     console.log("DB connection failed", err);
@@ -86,6 +42,3 @@ app.use((err, req, res, next) => {
     statusCode,
   });
 });
-
-// Export the Express app wrapped with serverless-http
-export const handler = serverless(app);
