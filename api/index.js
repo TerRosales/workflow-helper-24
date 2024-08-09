8f0e5a401ac3d4ae5a48b13206c0df69397daea6
Terrence Rosales <118107221+Romo3@users.noreply.github.com>
2024-08-09 10:36:17 -0400

    deployment config

--- api/index.js
+++ api/index.js
@@ -1,3 +1,48 @@
+// import express from "express";
+// import dotenv from "dotenv";
+// import mongoose from "mongoose";
+// import userRoutes from "./routes/user.routes.js";
+// import authRoutes from "./routes/auth.routes.js";
+// import lineRoutes from "./routes/line.routes.js";
+// import productRoutes from "./routes/product.routes.js";
+// import cookieParser from "cookie-parser";
+// import cors from "cors";
+// dotenv.config();
+
+// const app = express();
+// app.use(cors());
+// app.use(express.json());
+// app.use(cookieParser());
+
+// mongoose
+//   .connect(process.env.MONGODB)
+//   .then(() => {
+//     console.log("DB connected");
+//     app.listen(process.env.PORT, () => {
+//       console.log(`Server is running on port ${process.env.PORT}`);
+//     });
+//   })
+//   .catch((err) => {
+//     console.log("DB connection failed", err);
+//   });
+
+// app.use("/api/user", userRoutes);
+// app.use("/api/auth", authRoutes);
+// app.use("/api/lines", lineRoutes);
+// app.use("/api/product", productRoutes);
+
+// app.use((err, req, res, next) => {
+//   const statusCode = err.statusCode || 500;
+//   const message = err.message || "Internal Server Error";
+//   res.status(statusCode).json({
+//     success: false,
+//     message,
+//     statusCode,
+//   });
+// });
+
+// DEPLOYMNENT
+
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
@@ -7,6 +52,8 @@ import lineRoutes from "./routes/line.routes.js";
 import productRoutes from "./routes/product.routes.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import serverless from "serverless-http"; // Import serverless-http to handle Express app
+
 dotenv.config();
 
 const app = express();
@@ -18,9 +65,6 @@ mongoose
   .connect(process.env.MONGODB)
   .then(() => {
     console.log("DB connected");
-    app.listen(process.env.PORT, () => {
-      console.log(`Server is running on port ${process.env.PORT}`);
-    });
   })
   .catch((err) => {
     console.log("DB connection failed", err);
@@ -41,62 +85,5 @@ app.use((err, req, res, next) => {
   });
 });
 
-// GET - Read all Users
-// POST - Create a User
-// PUT - Update a User
-// DELETE - Delete a User
-
-// GET - Read all Lines
-// POST - Create a Line
-// PUT - Update a Line
-// DELETE - Delete a Line
-
-// GET - Read one Products
-// POST - Create a Product
-// PUT - Update a Product
-// DELETE - Delete a Product
-
-// Bottom snippet works for nodemailer
-// import nodemailer from "nodemailer";
-// import { fileURLToPath } from "url";
-// import path from "path";
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
-
-// const transporter = nodemailer.createTransport({
-//   service: "gmail",
-//   host: "smtp.gmail.com",
-//   port: 465,
-//   secure: true,
-//   auth: {
-//     user: process.env.EMAIL,
-//     pass: process.env.PASSWORD,
-//   },
-// });
-
-// const mailOptions = {
-//   from: {
-//     name: "TR-DEV",
-//     address: process.env.EMAIL,
-//   }, // sender address
-//   to: "gummBOOM2@gmail.com", // list of receivers
-//   subject: "Email Verification", // Subject line
-//   text: "Hello world?", // plain text body
-//   html: `<b>Email from Workflow Helper</b><br/><a href="https://thai-rest.tr-dev.dev/" target="_blank">hi</a>`, // html body
-//   attachments: {
-//     filename: "test.jpg",
-//     path: path.join(__dirname, "./utils/test.jpg"),
-//     contentType: "image/jpeg",
-//   },
-// };
-
-// const sendMail = async (transporter, mailOptions) => {
-//   try {
-//     await transporter.sendMail(mailOptions);
-//     console.log("Email sent");
-//   } catch (error) {
-//     console.error("Error: ", error);
-//   }
-// };
-
-// sendMail(transporter, mailOptions);
+// Export the Express app wrapped with serverless-http
+export const handler = serverless(app);
